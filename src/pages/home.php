acb817350a643ef52a12688785f47239006acf14
Kenny Wilson <kenny@digital-inflection.com>
2018-04-23 15:43:47 -0400

    fix type

--- src/pages/home.php
+++ src/pages/home.php
@@ -1,11 +1,11 @@
 <?php
-    get_header();
-    get_menu();
-    get_banner();
-    get_article_list(4, 0, "state");
-    get_4h_marketing();
-    get_marketing();
-    get_event_list(5, 0);
-    get_resource_links();
-    get_footer();
+get_header();
+get_menu();
+get_banner();
+get_article_list(4, 0, "state");
+get_4h_marketing();
+get_marketing();
+get_event_list(5, 0);
+get_resource_links();
+get_footer();
 ?>
