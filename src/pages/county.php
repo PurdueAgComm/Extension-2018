923e83f6141cbb649863bb6e166f7f3f2c4855f7
Kenny Wilson <kenny@digital-inflection.com>
2018-04-23 15:41:16 -0400

    update home and county pages with only content relevant to those pages

--- src/pages/county.php
+++ src/pages/county.php
@@ -16,44 +16,12 @@ $homepath = 'extension.purdue.edu/'.$county;
 bootstrap();
 
 /** End Bootstrap */
+get_header();
+get_menu();
+get_banner();
+get_article_list(4, 0, "county");
+get_marketing();
+get_event_list(5, 0);
+get_resource_links();
+get_footer();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-
-</head>
-<body>
-<div id="header">
-    <h2>Menu Example</h2>
-    <?php get_menu(); ?>
-</div>
-
-<div class="content">
-
-    <div id="banner">
-        <h2>Banner Example</h2>
-        <?php get_banner(); ?>
-    </div>
-
-    <div id="articles">
-        <h2>Articles Example</h2>
-        <?php get_article_list(7, 0); ?>
-    </div>
-
-    <div id="navigation">
-        <h2>Navigation Example</h2>
-        <?php get_navigation(); ?>
-    </div>
-
-    <div id="events">
-        <h2>Events</h2>
-        <?php get_event_list(5, 0); ?>
-    </div>
-
-</div>
-
-<div id="footer">
-    <?php get_footer(); ?>
-</div>
-</body>
-</html>
--- src/pages/home.php
+++ src/pages/home.php
@@ -3,7 +3,7 @@
     get_menu();
     get_banner();
     get_article_list(4, 0, "state");
-    get_article_list(3, 0, "county");
+    get_4h_marketing();
     get_marketing();
     get_event_list(5, 0);
     get_resource_links();
