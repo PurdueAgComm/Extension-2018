0237565931870f479ec36623db40f146934d36e1
Kenny Wilson <kenny@digital-inflection.com>
2018-04-23 11:28:20 -0400

    add 4h marker

--- src/partials/feed-state-articles.php
+++ src/partials/feed-state-articles.php
@@ -3,14 +3,16 @@
  * Feed item/article markup
  */
 ?>
-
 <div class="container">
   <div class="story-callouts">
     <h2 class="story-callouts__title">Recent Stories</h2>
     <div class="row no-gutters">
       <?php foreach($articles as $article):?>
         <a href="/article/<?php echo $article->intItemID; ?>" class="col-md-3 story-callouts__callout">
-          <p class="story-callouts__callout-date"><?php echo $article->datCreated; ?></p>
+          <p class="story-callouts__callout-date"><?php echo $article->datCreated; ?>
+            FakeDate 30, 2018
+            <?php if($article->bln4H) : echo " | 4-H"; endif; ?>
+          </p>
           <h3 class="story-callouts__callout-title"><?php echo $article->strTitle; ?></h3>
         </a>
       <?php endforeach; ?>
@@ -18,5 +20,3 @@
     </div>
   </div>
 </div>
-
-<?php print_r($articles); ?>
