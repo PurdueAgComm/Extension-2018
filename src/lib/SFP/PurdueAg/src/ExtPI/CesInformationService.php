5a8a5377ff8c43e8c8e7c4a5507564cc3a237f64
John Alder <jack@jalder.com>
2018-05-11 16:47:07 -0400

    prototyping the ExtPI library classes

--- src/lib/SFP/PurdueAg/src/ExtPI/CesInformationService.php
+++ src/lib/SFP/PurdueAg/src/ExtPI/CesInformationService.php
@@ -2,20 +2,32 @@
 
 namespace SFP\PurdueAg\ExtPI;
 
-class CesInformationService
+require_once('Call.php');
+
+class CesInformationService extends Call
 {
+    private $call;
+
+    public function __construct()
+    {
+        $this->call = $this->baseurl.'CesInformationService.svc/json/';
+    }
+
     public function getCesJobList()
     {
-
+        $this->url = $this->call.'GetCesJobList';
+        return $this->get();
     }
 
     public function getTitleCesList()
     {
-
+        $this->url = $this->call.'GetTitleCesList';
+        return $this->get();
     }
 
     public function getVersion()
     {
-
+        $this->url = $this->call.'Version';
+        return $this->get();
     }
 }
--- src/lib/SFP/PurdueAg/src/ExtPI/CountyService.php
+++ src/lib/SFP/PurdueAg/src/ExtPI/CountyService.php
@@ -2,25 +2,38 @@
 
 namespace SFP\PurdueAg\ExtPI;
 
-class CountyService
+require_once('Call.php');
+
+class CountyService extends Call
 {
+    private $call;
+
+    public function __construct()
+    {
+        $this->call = $this->baseurl.'CountyService.svc/json/';
+    }
+
     public function getCounty($intCountyID = 0)
     {
-
+        $this->url = $this->call.'GetCounty?intCountyID='.$intCountyID;
+        return $this->get();
     }
 
     public function getCountyList()
     {
-
+        $this->url = $this->call.'GetCountyList';
+        return $this->get();
     }
 
     public function getStaffDirectory($intCountyID)
     {
-
+        $this->url = $this->call.'GetStaffDirectory?intCountyID='.$intCountyID;
+        return $this->get();
     }
 
     public function getVersion()
     {
-
+        $this->url = $this->call.'Version';
+        return $this->get();
     }
 }
