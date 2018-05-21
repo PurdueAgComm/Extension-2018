50d354f18ec84ad30e8bcc39b8f021f3563dcab0
Kenny Wilson <kenny@digital-inflection.com>
2018-05-21 15:39:24 -0400

    label page styling

--- src/partials/page-label.php
+++ src/partials/page-label.php
@@ -1,10 +1,18 @@
-<h1><?php echo $label->strLabel; ?></h1>
-<ul>
-<?php foreach($list as $article): ?>
-    <li>
-        <?php //todo: set the url base to the county so users stay "on-site" when linking? ?>
-        <a href="/article/<?php echo $article->intItemID; ?>"><?php echo $article->strTitle; ?></a>
-        <?php var_dump($article); ?>
-    </li>
-<?php endforeach; ?>
-</ul>
+<div class="wide-container no-margin-auto label__title--wide--background">
+  <div class="container label__title--background">
+    <h1 class="label__title reveal">Articles tagged: <?php echo $label->strLabel; ?></h1>
+  </div>
+</div>
+<div class="container">
+  <ul class="label__list">
+  <?php foreach ($list as $article): ?>
+      <a class="label" href="/article/<?php echo $article->intItemID; ?>">
+        <li class="label__list-item">
+          <?php //todo: set the url base to the county so users stay "on-site" when linking??>
+          <h2><?php echo $article->strTitle; ?></h2>
+          <p><?php echo $article->strShortBody; ?></p>
+        </li>
+      </a>
+  <?php endforeach; ?>
+  </ul>
+</div>
