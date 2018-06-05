bc8947c2baa845207a70149704a6f55e62446637
Kenny Wilson <kenny@digital-inflection.com>
2018-06-05 15:58:45 -0400

    add diagonal and update give link

--- src/partials/tpl-give.php
+++ src/partials/tpl-give.php
@@ -1,12 +1,12 @@
-<div class="wide-container">
-  <div class="give">
+<div class="wide-container" style="margin-top: 3rem;">
+  <div class="give edge edge--top--reverse">
     <div class="container">
       <div class="row">
         <div class="col">
           <h2 class="give__headline">Help move Purdue Extension forward</h2>
           <p>Every dollar you donate supports our efforts to build vibrant communities, strong families, and profitable businesses on its Main Streets and at its farm gates – regularly reaching more than 620,000 people. Our team – including campus specialists and Extension Educators in all 92 counties – connects Indiana communities to world-class Purdue University research and a nationwide network of experts on agriculture, community, environment, family, food, and youth.</p>
           <div class="give__cta">
-            <a href="" class="cta cta__secondary cta__secondary--black">
+            <a href="https://secure.ud.purdue.edu/s/1461/campaign/give.aspx?sid=1461&gid=1010&pgid=3175&cid=7440&bledit=1&sort=1&dids=5355.35&appealcode=15394" class="cta cta__secondary cta__secondary--black">
               Give Now <i class="fas fa-arrow-alt-circle-right"></i>
             </a>
           </div>
