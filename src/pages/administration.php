2c173fc0405f45851fba03dc9a0a96ddb59e8fd5
Kenny Wilson <kenny@digital-inflection.com>
2018-08-06 10:14:50 -0400

    template file for admin page

---
+++ src/pages/administration.php
@@ -0,0 +1,7 @@
+<?php
+get_header();
+get_menu();
+get_banner();
+get_administration();
+get_resource_links();
+get_footer();
---
+++ src/partials/page-administration.php
@@ -0,0 +1,32 @@
+<div class="wide-container no-margin-auto story__title--wide--background">
+  <div class="container story__title--background">
+    <h1 class="story__title reveal">Meet the Administration</h1>
+  </div>
+</div>
+<br>
+<div class="container">
+  <div class="row">
+    <div class="col-md-2">
+      <img src="https://extension.purdue.edu/ProfileImages/hender65.jpg" alt="Jason Henderson">
+    </div>
+    <div class="col-md-4">
+      <h2>Jason Henderson</h2>
+      <p>Director, Purdue Extension<br>
+        Associate Dean of Purdue Agriculture<br>
+        765-494-8489<br>
+        <a href="mailto:jhenderson@purdue.edu">jhenderson@purdue.edu</a>
+      </p>
+    </div>
+    <div class="col-md-2">
+      <img src="https://extension.purdue.edu/ProfileImages/hender65.jpg" alt="Jason Henderson">
+    </div>
+    <div class="col-md-4">
+      <h2>Jason Henderson</h2>
+      <p>Director, Purdue Extension<br>
+        Associate Dean of Purdue Agriculture<br>
+        765-494-8489<br>
+        <a href="mailto:jhenderson@purdue.edu">jhenderson@purdue.edu</a>
+      </p>
+    </div>
+  </div> <!-- /.row -->
+</div>
