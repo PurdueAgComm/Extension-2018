68634926d0f5662e0cfbc24c4d47223c169d88d3
Kenny Wilson <kenny@digital-inflection.com>
2018-08-06 14:40:19 -0400

    administration page complete

--- src/partials/page-administration.php
+++ src/partials/page-administration.php
@@ -5,6 +5,469 @@
 </div>
 <br>
 <div class="container">
-  Coming soon to a container near you.
+  <div class="col-12">
+    <br>
+    <h2>Extension Leadership Team</h2>
+  </div>
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/hender65.jpg" alt="Jason Henderson">
+      </div>
+      <div class="col">
+        <h3>Jason Henderson</h3>
+        <p class="profile-meta"> Director, Purdue Extension<br> Associate Dean of Purdue Agriculture<br>
+          <i class="fas fa-mobile-alt fa-fw"></i> 765-494-8489<br>
+          <i class="fas fa-envelope fa-fw"></i> <a href="mailto:jhenderson@purdue.edu">jhenderson@purdue.edu</a>
+        </p>
+      </div>
+    </div>
+  </div>
 
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/abbottar.jpg" alt="Angela Abbott">
+      </div>
+      <div class="col">
+        <h3>Angela Abbott</h3>
+        <p class="profile-meta"> Associate Director, Purdue Extension<br> Program Leader, Health &amp; Human Sciences<br>
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:7654962488">765-496-2488</a><br>
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:abbottar@purdue.edu">abbottar@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/hender65.jpg" alt="Jason Henderson">
+      </div>
+      <div class="col">
+        <h3>Lisa Anderson</h3>
+        <p class="profile-meta"> Northwest District Director, Purdue Extension<br>
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:57477730267">574-773-0267</a><br>
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:schroedd@purdue.edu">ande1028@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/lbeaulie.jpg" alt="Lionel J. (Bo) Beaulieu">
+      </div>
+      <div class="col">
+        <h3>Lionel J. (Bo) Beaulieu</h3>
+        <p class="profile-meta"> Assistant Director, Purdue Extension<br> Program Leader, Community Development<br> Director, Purdue Center for Regional Development<br>
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:7654944277">765-494-4277</a><br>
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:ljb@purdue.edu">ljb@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/gray2.jpg" alt="Julie Gray">
+      </div>
+      <div class="col">
+        <h3>Julie Gray</h3>
+        <p class="profile-meta"> Central District Director, Purdue Extension<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:3173868919">317-386-8919</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:juliegray@purdue.edu">juliegray@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/hartja.jpg" alt="Julie Hart">
+      </div>
+      <div class="col">
+        <h3>Julie Hart</h3>
+        <p class="profile-meta"> Southwest District Director, Purdue Extension<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:8122324643">812-232-4643</a><br />
+          <i class="fas fa-envelope fa-fw"></i> <a href="mailto:hartja@purdue.edu">hartja@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/pmarcell.jpg" alt="Paul Marcellino">
+      </div>
+      <div class="col">
+        <h3>Paul Marcellino</h3>
+        <p class="profile-meta"> East District Director, Purdue Extension<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:2603754440">260-375-4440</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:pmarcellino@purdue.edu">pmarcellino@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/rmckee.jpg" alt="Renee McKee">
+      </div>
+      <div class="col">
+        <h3>Renée McKee</h3>
+        <p class="profile-meta"> Assistant Director, Purdue Extension<br /> Program Leader, Indiana 4-H Youth Development<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:7654948422">765-494-8422</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:rmckee@purdue.edu">rmckee@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/niermana.jpg" alt="Amy Nierman">
+      </div>
+      <div class="col">
+        <h3>Amy Nierman</h3>
+        <p class="profile-meta"> Southeast District Director, Purdue Extension<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:8123583887">812-358-3887</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:niermana@purdue.edu">niermana@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/fosterre.jpg" alt="Rick Foster">
+      </div>
+      <div class="col">
+        <h3>Rick Foster</h3>
+        <p class="profile-meta"> Interim Assistant Director, Purdue Extension<br /> Interim Program Leader, Agriculture &amp; Natural Resources<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:7654948490">765-494-8490</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:rfoster@purdue.edu">rfoster@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-12">
+    <br>
+    <h2>Purdue Extension Coordinators</h2>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/kfarthin.jpg" alt="Krista Farthing">
+      </div>
+      <div class="col">
+        <h3>Krista Farthing</h3>
+        <p class="profile-meta"> Staff Development Coordinator<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:7654966515">765-496-6515</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:kfarthin@purdue.edu">kfarthin@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/jhuettem.jpg" alt="Julie Huetteman">
+      </div>
+      <div class="col">
+        <h3>Julie Huetteman</h3>
+        <p class="profile-meta"> Strategic Initiatives Coordinator<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:7654948309">765-494-8309</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:jhuettem@purdue.edu">jhuettem@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/glindber.jpg" alt="Greg Lindberg">
+      </div>
+      <div class="col">
+        <h3>Greg Lindberg</h3>
+        <p class="profile-meta"> IT Specialist<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:7654960350">765-496-0350</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:glindber@purdue.edu">glindber@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/rwynkoop.jpg" alt="Ryan Wynkoop">
+      </div>
+      <div class="col">
+        <h3>Ryan Wynkoop</h3>
+        <p class="profile-meta"> IT Specialist<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:7654949516">765-494-9516</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:rwynkoop@purdue.edu">rwynkoop@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-12">
+    <br>
+    <h2>Agriculture & Natural Resources Extension</h2>
+  </div>
+
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/fosterre.jpg" alt="Rick Foster">
+      </div>
+      <div class="col">
+        <h3>Rick Foster</h3>
+        <p class="profile-meta"> Interim Program Leader, Agriculture &amp; Natural Resources<br /> Interim Assistant Director, Purdue Extension<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:7654948490">765-494-8490</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:rfoster@purdue.edu">rfoster@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/tamara17.jpg" alt="Tamara Benjamin">
+      </div>
+      <div class="col">
+        <h3>Tamara Benjamin</h3>
+        <p class="profile-meta"> Assistant Program Leader, Agriculture &amp; Natural Resources<br /> ANR Diversified Agriculture Specialist<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:7654961930">765-496-1930</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:tamara17@purdue.edu">tamara17@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/gbossaer.jpg" alt="Greg Bossaer">
+      </div>
+      <div class="col">
+        <h3>Greg Bossaer</h3>
+        <p class="profile-meta"> Assistant Program Leader, Agriculture &amp; Natural Resources<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:7654948490">765-494-8490</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:gbossaer@purdue.edu">gbossaer@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/jellett.jpg" alt="Jodee Ellett">
+      </div>
+      <div class="col">
+        <h3>Jodee Ellett</h3>
+        <p class="profile-meta"> Local Foods Program Coordinator<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:7654940349">765-494-0349</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:jellett@purdue.edu">jellett@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/jsmonroe.jpg" alt="James Scott Monroe">
+      </div>
+      <div class="col">
+        <h3>James Scott Monroe</h3>
+        <p class="profile-meta"> Food Safety Extension Educator<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:8128860198">812-886-0198</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:jsmonroe@purdue.edu">jsmonroe@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/modonnel.jpg" alt="Michael O'Donnell">
+      </div>
+      <div class="col">
+        <h3>Michael O'Donnell</h3>
+        <p class="profile-meta"> Extension Educator in Organic and Diversified Agriculture<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:7652848414">765-284-8414</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:modonnel@purdue.edu">modonnel@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/wsell.jpg" alt="Walt Sell">
+      </div>
+      <div class="col">
+        <h3>Walt Sell</h3>
+        <p class="profile-meta"> Assistant Program Leader, Agriculture &amp; Natural Resources<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:5742018065">574-201-8065</a> (cell)<br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:wsell@purdue.edu">wsell@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-12">
+    <br>
+    <h2>Community Development Extension</h2>
+  </div>
+  <br />
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/lbeaulie.jpg" alt="Lionel J. (Bo) Beaulieu">
+      </div>
+      <div class="col">
+        <h3>Lionel J. (Bo) Beaulieu</h3>
+        <p class="profile-meta"> Program Leader, Community Development<br /> Director, Purdue Center for Regional Development<br /> Assistant Director, Purdue Extension<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:7654944277">765-494-4277</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:ljb@purdue.edu">ljb@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/wilcox16.jpg" alt="Michael Wilcox">
+      </div>
+      <div class="col">
+        <h3>Michael Wilcox</h3>
+        <p class="profile-meta"> Assistant Program Leader, Community Development<br /> Senior Associate, Purdue Center for Regional Development<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:7654962640">765-496-2640</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:wilcox16@purdue.edu">wilcox16@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-12">
+    <br>
+    <h2>Health &amp; Human Sciences Extension</h2>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/abbottar.jpg" alt="Angela Abbott">
+      </div>
+      <div class="col">
+        <h3>Angela Abbott</h3>
+        <p class="profile-meta"> Program Leader, Health &amp; Human Sciences<br /> Associate Director, Purdue Extension<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:7654962488">765-496-2488</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:abbottar@purdue.edu">abbottar@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/lgraves.jpg" alt="Lisa Graves">
+      </div>
+      <div class="col">
+        <h3>Lisa Graves</h3>
+        <p class="profile-meta"> Assistant Program Leader, Health &amp; Human Sciences<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:7654942828">765-494-2828</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:lgraves@purdue.edu">lgraves@purdue.edu</a></p>
+      </div>
+    </div>
+  </div><br />
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/swoodcox.jpg" alt="Stephanie Woodcox">
+      </div>
+      <div class="col">
+        <h3>Stephanie Woodcox</h3>
+        <p class="profile-meta"> Assistant Program Leader, Health &amp; Human Sciences<br /> HHS Extension Specialist - Health and Wellness<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:7654948523">765-494-8523</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:swoodcox@purdue.edu">swoodcox@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-12">
+    <br>
+    <h2>Indiana 4-H Youth Development Extension</h2>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/rmckee.jpg" alt="Renée McKee">
+      </div>
+      <div class="col">
+        <h3>Renée McKee</h3>
+        <p class="profile-meta"> Program Leader, Indiana 4-H Youth Development<br /> Assistant Director, Purdue Extension<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:7654948422">765-494-8422</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:rmckee@purdue.edu">rmckee@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/carrella.jpg" alt="Tony Carrell">
+      </div>
+      <div class="col">
+        <h3>Tony Carrell</h3>
+        <p class="profile-meta"> Indiana 4-H Youth Development Extension Specialist - Staff Development, Trips, and Awards<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:7654948435">765-494-8435</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:carrella@purdue.edu">carrella@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/fisher86.png" alt="Aaron Fisher">
+      </div>
+      <div class="col">
+        <h3>Aaron Fisher</h3>
+        <p class="profile-meta"> Indiana 4-H Youth Development Extension Specialist - Youth Livestock<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:7654948435">765-494-8435</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:fisher86@purdue.edu">fisher86@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/alfrost.jpg" alt="Angie Frost">
+      </div>
+      <div class="col">
+        <h3>Angie Frost</h3>
+        <p class="profile-meta"> Indiana 4-H Youth Development Extension Specialist - Healthy Living<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:7654948435">765-494-8435</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:alfrost@purdue.edu">alfrost@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-6 profile-admin">
+    <div class="row">
+      <div class="col-4">
+        <img src="https://extension.purdue.edu/ProfileImages/mckinles.jpg" alt="Steve McKinley">
+      </div>
+      <div class="col">
+        <h3>Steve McKinley</h3>
+        <p class="profile-meta"> Indiana 4-H Youth Development Extension Specialist - Leadership and Volunteerism<br />
+          <i class="fas fa-mobile-alt fa-fw"></i><a href="tel:7654948435">765-494-8435</a><br />
+          <i class="fas fa-envelope fa-fw"></i><a href="mailto:mckinles@purdue.edu">mckinles@purdue.edu</a></p>
+      </div>
+    </div>
+  </div>
 </div>
