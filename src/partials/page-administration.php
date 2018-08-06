c7187498e0a61bb9def3073a4b3c924b27c27130
Kenny Wilson <kenny@digital-inflection.com>
2018-08-06 10:46:48 -0400

    style updates for admin page

--- src/partials/page-administration.php
+++ src/partials/page-administration.php
@@ -5,28 +5,6 @@
 </div>
 <br>
 <div class="container">
-  <div class="row">
-    <div class="col-md-2">
-      <img src="https://extension.purdue.edu/ProfileImages/hender65.jpg" alt="Jason Henderson">
-    </div>
-    <div class="col-md-4">
-      <h2>Jason Henderson</h2>
-      <p>Director, Purdue Extension<br>
-        Associate Dean of Purdue Agriculture<br>
-        765-494-8489<br>
-        <a href="mailto:jhenderson@purdue.edu">jhenderson@purdue.edu</a>
-      </p>
-    </div>
-    <div class="col-md-2">
-      <img src="https://extension.purdue.edu/ProfileImages/hender65.jpg" alt="Jason Henderson">
-    </div>
-    <div class="col-md-4">
-      <h2>Jason Henderson</h2>
-      <p>Director, Purdue Extension<br>
-        Associate Dean of Purdue Agriculture<br>
-        765-494-8489<br>
-        <a href="mailto:jhenderson@purdue.edu">jhenderson@purdue.edu</a>
-      </p>
-    </div>
-  </div> <!-- /.row -->
+  Coming soon to a container near you.
+
 </div>
