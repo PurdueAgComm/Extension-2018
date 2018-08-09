f8c7d2cb6f52855ab2c091febd71d606e92e1a41
Kenny Wilson <kenny@digital-inflection.com>
2018-08-09 14:55:11 -0400

    remove extra function and add GA

--- src/pages/home.php
+++ src/pages/home.php
@@ -5,7 +5,6 @@ get_banner();
 get_article_list(4, 0, "state");
 get_give();
 get_marketing();
-// get_event_list(5, 0, "home");
 get_event_filter(5, 0, "home");
 get_resource_links();
 get_footer();
