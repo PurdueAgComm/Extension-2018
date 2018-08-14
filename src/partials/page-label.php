3b2e659e4a7f2844c2581df26b4b9c1ee0b8bf92
Kenny Wilson <kenny@digital-inflection.com>
2018-08-14 14:49:16 -0400

    empty state for label page

--- src/partials/page-label.php
+++ src/partials/page-label.php
@@ -1,3 +1,4 @@
+<?php global $county; ?>
 <div class="wide-container no-margin-auto label__title--wide--background">
   <div class="container label__title--background">
     <h1 class="label__title reveal">Articles tagged: <?php echo $label->strLabel; ?></h1>
@@ -5,13 +6,18 @@
 </div>
 <div class="container">
   <ul class="label__list">
-  <?php foreach ($list as $article): ?>
-      <a class="label" href="<?php echo $county ? '/'.$county : ''; ?>/article/<?php echo $article->intItemID; ?>">
-        <li class="label__list-item">
-          <h2><?php echo $article->strTitle; ?></h2>
-          <p><?php echo $article->strShortBody; ?></p>
-        </li>
-      </a>
-  <?php endforeach; ?>
+  <?php if(!isset($list)) : ?>
+    <?php foreach ($list as $article): ?>
+        <a class="label" href="<?php echo $county ? '/'.$county : ''; ?>/article/<?php echo $article->intItemID; ?>">
+          <li class="label__list-item">
+            <h2><?php echo $article->strTitle; ?></h2>
+            <p><?php echo $article->strShortBody; ?></p>
+          </li>
+        </a>
+    <?php endforeach; ?>
+  <?php else : ?>
+    <br>
+    <div class="alert alert-secondary" role="alert" style='color: #383d41;'><i class="fas fa-info-circle"></i> It looks like there are no articles tagged with this label. Go back, <a style='color: #383d41; text-decoration: underline;' href="<?php echo !empty($county) ? '/' . $county : ''; ?>/about/">contact us</a>, or search for what you're looking for above.</div>
+  <?php endif; ?>
   </ul>
 </div>
